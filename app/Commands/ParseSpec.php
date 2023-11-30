fc534e643db2b02ebc007f0a46065b227a017a89
Chris Reed <chris@reedtech.us>
2023-11-30 15:54:28 -0600

    feat(api): enhance spec file parsing and error handling
    The ParseSpec command now retrieves the spec file path from the cache. If the path is not set, the command fails with an error message. The command now also checks if the spec file exists before parsing. If the spec file does not exist, the command fails with an error message.

--- app/Commands/ParseSpec.php
+++ app/Commands/ParseSpec.php
@@ -3,6 +3,7 @@
 namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Cache;
 use LaravelZero\Framework\Commands\Command;
 
 class ParseSpec extends Command
@@ -12,7 +13,7 @@ class ParseSpec extends Command
      *
      * @var string
      */
-    protected $signature = 'parse-spec {spec}';
+    protected $signature = 'spec:parse {spec : The spec file to parse.}';
 
     /**
      * The description of the command.
@@ -28,9 +29,25 @@ class ParseSpec extends Command
      */
     public function handle()
     {
+        $specPath = Cache::get('spec:path');
+        if (!$specPath) {
+            $this->error('No spec path set.');
+            return self::FAILURE;
+        }
+
         $spec = $this->argument('spec');
-        $this->info("Parsing spec file: $spec");
-        $this->info("Generating test file: $spec");
+        $filename = $spec . '.json';
+        $fullSpecPath = $specPath . '/' . $filename;
+
+        $this->info("Parsing spec file: $fullSpecPath");
+        $this->info("Generating test file: output/$spec");
+
+        if (!file_exists($fullSpecPath)) {
+            $this->error("Spec file not found: $fullSpecPath");
+            return self::FAILURE;
+        }
+
+        return self::SUCCESS;
     }
 
     /**
